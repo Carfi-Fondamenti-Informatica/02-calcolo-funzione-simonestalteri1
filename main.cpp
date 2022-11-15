650ad401326972de7d52cf952709e83467fd51b3
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-11-15 11:59:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   return 0;
+#include <cmath>
+int main()
+{
+   float a,b,x,y;
+   cin>>a;
+   cin>>b;
+   cin>>x;
+   cin>>y;
+   if(x<0 && y>0)
+   {
+       cout<<a*x - b*y<<endl;
+   }
+   else if (x >= 0 && y <= 0)
+    {
+        cout<<a*pow(x, 2) - b*y<<endl;
+    }
+   else
+    {
+        cout<<a*x+b*pow(y,2)<<endl;
+    }
+    return 0;
 }
